2523d08ce55b02ec2159b262ea0bcc36b84c5011
leshchenko1979 <60138889+leshchenko1979@users.noreply.github.com>
2020-07-24 15:37:51 +0300

    Bitrix class and Bitrix.get_list() added

--- lxutils/bitrix.py
+++ lxutils/bitrix.py
@@ -1,5 +1,93 @@
 import urllib.parse
 
+import asyncio
+import aiohttp
+import time
+
+from tqdm import tqdm
+
+class SemaphoreWrapper():
+
+    def __init__(self, custom_pool_size=100):
+        self._stopped_time = None
+        self._stopped_value = None
+        self._REQUESTS_PER_SECOND = 2
+        self._pool_size = custom_pool_size
+
+
+    async def __aenter__(self):
+        self._sem = asyncio.BoundedSemaphore(self._pool_size)
+        if self._stopped_time:
+            self._value = min(self._sem._bound_value, self._stopped_value + 
+                int((time.monotonic() - self._stopped_time) * self._REQUESTS_PER_SECOND))
+            self._stopped_time = None
+            self._stopped_value = None
+        self.release_task = asyncio.create_task(self._release_sem())
+
+
+    async def __aexit__(self, a1, a2, a3):
+        self._stopped_time = time.monotonic()
+        self._stopped_value = self._sem._value
+        self.release_task.cancel()
+
+
+    async def _release_sem(self):
+        while True:
+            if self._sem._value < self._sem._bound_value - 1:
+                self._sem.release()
+            await asyncio.sleep(1 / self._REQUESTS_PER_SECOND)
+
+
+    async def acquire(self):
+        return await self._sem.acquire()
+
+class Bitrix:
+    def __init__(self, webhook, custom_pool_size = 100):
+        self.webhook = webhook
+        self._sw = SemaphoreWrapper(custom_pool_size)
+
+
+    async def _get_paginated_list(self, method, payload_details=None, custom_start=0):
+        async with self._sw, aiohttp.ClientSession(raise_for_status=True) as session:
+            results, total = await self._get_list_starting(session, custom_start, method, payload_details)
+            tasks = []
+            if not total or total <= 50:
+                return results
+            with tqdm(total=total, initial=custom_start + len(results)) as pbar:
+                for start in range(custom_start + len(results), total, 50):
+                    tasks.append(asyncio.create_task(self._get_list_starting(
+                        session, start, method, payload_details, pbar)))
+                for x in asyncio.as_completed((*tasks, self._sw.release_task)):
+                    r = await x
+                    results.extend(r[0])
+                    if len(results) >= total: break
+            results = [dict(t) for t in {tuple(d.items()) for d in results}] 
+            if len(results) == total: 
+                return results
+#            else:
+#                raise RuntimeWarning (
+#                    f'Got {len(results)} entries, while expecting {total}')
+        
+
+    async def _get_list_starting(self, session, start, method, payload_details=None, pbar=None):
+        url = self.webhook + method
+
+        # log (f'Downloading list "{method}": {start}')
+        payload = [('start', start)] + \
+            (payload_details if payload_details else [])
+
+        await self._sw.acquire()
+        async with session.get(url, params=payload) as response:
+            r = await response.json(encoding='utf-8')
+        if pbar:
+            pbar.update(len(r['result']))
+        return r['result'], (r['total'] if 'total' in r.keys() else None)
+
+
+    def get_list(self, method, payload_details=None):
+        return asyncio.run(self._get_paginated_list(method, payload_details))
+
+
 def http_build_query(data):
     parents = list()
     pairs = dict()
--- lxutils/test_utils.py
+++ lxutils/test_utils.py
@@ -1,6 +1,33 @@
 from lxutils import *
 import doctest
+import time
+import pytest
 
 def test_timer():
     failures, __ = doctest.testmod(name = 'lxutils.log')
     assert failures == 0
+
+@pytest.fixture(scope='module')
+def get_bitrix():
+    return Bitrix("https://ctrlcrm.bitrix24.ru/rest/1/0agnq1xt4xv1cqnc/")
+
+
+def test_bitrix(get_bitrix):
+    b = get_bitrix
+    deals = b.get_list('crm.deal.list')
+    assert len(deals) > 1000
+
+def test_bitrix_semaphore_start_stop(get_bitrix):
+    b = get_bitrix
+    t1 = time.monotonic()
+    deals1 = b.get_list('crm.deal.list')
+    assert b._sw._sem._value < 100
+    t2 = time.monotonic()
+    deals2 = b.get_list('crm.deal.list')
+    t3 = time.monotonic()
+    assert t3 - t2 < t2 - t1
+
+def test_bitrix_short_lists(get_bitrix):
+    b = get_bitrix
+    assert len(b.get_list('crm.dealcategory.stage.list')) <= 50
+    assert len(b.get_list('user.get')) <= 50
